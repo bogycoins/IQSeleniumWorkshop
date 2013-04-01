b67f78241bddd724f49e2862cf330a4d022399a3
Vallach <vallach@yahoo.ro>
2013-04-01 15:37:57 +0300

    Updated data model

--- src/datamodel/DataFilterNegociationsComponent.java
+++ src/datamodel/DataFilterNegociationsComponent.java
@@ -3,20 +3,60 @@ package datamodel;
 
 public class DataFilterNegociationsComponent {
 	
-	public String requestName;
-	public String negociationStatus;
-	public String licensingStatus;
-	public String startDate;
-	public String endDate;
+	private String requestName;
+	private String negociationStatus;
+	private String licensingStatus;
+	private String startDate;
+	private String endDate;
 	
+	public String getRequestName() {
+		return requestName;
+	}
+
+	public void setRequestName(String requestName) {
+		this.requestName = requestName;
+	}
+
+	public String getNegociationStatus() {
+		return negociationStatus;
+	}
+
+	public void setNegociationStatus(String negociationStatus) {
+		this.negociationStatus = negociationStatus;
+	}
+
+	public String getLicensingStatus() {
+		return licensingStatus;
+	}
+
+	public void setLicensingStatus(String licensingStatus) {
+		this.licensingStatus = licensingStatus;
+	}
+
+	public String getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+
+	public String getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+
 	public DataFilterNegociationsComponent(String requestName, String negociationStatus, String licensingStatus, 
 			String startDate, String endDate) {
 		
-		this.requestName = requestName;
-		this.negociationStatus = negociationStatus;
-		this.licensingStatus = licensingStatus;
-		this.startDate = startDate;
-		this.endDate = endDate;
+		setRequestName(requestName);
+		setNegociationStatus(negociationStatus);
+		setLicensingStatus(licensingStatus);
+		setStartDate(startDate);
+		setEndDate(endDate);
 	}
 
 }
